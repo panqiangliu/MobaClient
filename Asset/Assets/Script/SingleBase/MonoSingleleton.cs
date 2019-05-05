57b7bf76c96c7500cf5d1cde1248658b16e5f98f
v_panqliu <540651527@qq.com>
2019-05-05 21:09:06 +0800

    单例

---
+++ Asset/Assets/Script/SingleBase/MonoSingleleton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleleton<T> : MonoBehaviour where T:MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                if (GameObject.Find("ComSingleton") == null)
+                {
+                    GameObject go=new GameObject("ComSingleton");
+                    DontDestroyOnLoad(go);
+                }
+                if(GameObject.Find("ComSingleton") != null)
+                {
+                    if (GameObject.Find("ComSingleton").GetComponent<T>()!=null)
+                    {
+                        instance = GameObject.Find("ComSingleton").AddComponent<T>();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ Asset/Assets/Script/Tools/FileCom.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileCom
+{
+    //获取文件的内容
+    public static string GetFileText(string filePath)
+    {
+        string connect = string.Empty;
+        if (!File.Exists(filePath))
+        {
+            return connect;
+        }
+
+        //通过读取流，读取文本中的所有的内容
+        using (StreamReader sr = File.OpenText(filePath))
+        {
+            connect = sr.ReadToEnd();
+        }
+        return connect;
+    }
+
+    public static void CreatFile(string filePath)
+    {
+        string connect = string.Empty;
+        DeletFile(filePath);
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath);
+        }
+    }
+
+    public static void DeletFile(string filePath)
+    {
+
+    }
+
+}

6409adc14c373c08f73665744def3cbf40374082
panqiangliu <30800339+panqiangliu@users.noreply.github.com>
2019-05-11 17:59:34 +0800

    editor 文件夹创立 (#2)

---
+++ Asset/Assets/Script/Config/ConfigMgr.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigMgr : MonoSingleleton<ConfigMgr>
+{
+    public bool CheckIsABLoad()
+    {
+        return GameConfig.Instance.IsABLoad;
+    }
+}
---
+++ Asset/Assets/Script/Config/GameConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConfig :SingleletonBase<GameConfig>
+{
+    //判断是否通过AB的形式加载AssetNundle
+    public bool IsABLoad = false; 
+}
